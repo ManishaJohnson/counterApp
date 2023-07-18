1e30a4a8cd452d42e5015eda772ffdb82f5f8b15
Manisha Johnson <manishajohnson@MacBook-Pro.local>
2023-07-19 01:24:52 +0530

    settings.py

--- counterApp/settings.py
+++ counterApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 import whitenoise
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -118,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
